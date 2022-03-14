675ab84a1c8203f392151589bae0872163c03634
RhuanCastilho <101209955+RhuanCastilho@users.noreply.github.com>
2022-03-14 05:36:02 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,104 @@
+/* OPEN AND CLOSE MENU */
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+/*CLOSE MENU WHEN OPEN OTHER MENU*/
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+/*=========SCROLL W/ SHANDOW======= */
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= navHeight) {
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+})
+
+/*=========TESTEMONIALS======= */
+
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
+})
+
+/*===========SCROLLREVEAL==========*/
+
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .image, #home .text,
+  #about .image, #about .text,
+  #products header, #products.card,
+  #testimonials header, .swiper-wrapper,
+  #photos header, .swiper-wrapper,
+  #contatc .text, #contatc .links,
+  footer .brand, footer .social
+`,
+  { interval: 100 }
+)
+/*===========BACK-TOP========== */
+
+const backTopBottom = document.querySelector('.back-top')
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= 560) {
+    backTopBottom.classList.add('show')
+  } else {
+    backTopBottom.classList.remove('show')
+  }
+})
+
+/*===========MENU COLORIDO QUANDO NO LUGAR CERTO=============*/
+
+const sections = document.querySelectorAll('main section[id]')
+window.addEventListener('scroll', function () {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+})
